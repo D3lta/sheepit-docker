d94698f2358ef202ef67abcee7f1a2773fe80788
D3lta <D3lta@users.noreply.github.com>
2019-02-18 19:00:23 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 echo "Using: ${USER}:${PASS} with ${cores} cores and cache-dir: ${cache}"
 
-exec java -jar ${WORKDIR}/${NAME} -ui text -login ${USER} -password ${PASS} -cores ${cores} -cache-dir ${cache}
+exec java -jar ${WORKDIR}/${NAME} --verbose -ui text -login ${USER} -password ${PASS} -cores ${cores} -cache-dir ${cache}
